dba53bd7608bfb56ba5a61e4c10cda225f3fb6bc
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-09-08 16:44:18 -0400

    version 2 gameloop

--- sticks.py
+++ sticks.py
@@ -1,1 +1,45 @@
+import re
 
+
+def game_lopp(sticks):
+
+    pass
+
+
+
+
+def main():
+
+    sticks = int(input("Please select how many sticks you would like in the pile of stinks. \n Please enter a number between 10 and 100: "))
+
+
+    while True:
+      while sticks > 0:
+        player1_sticks = int(input("Player 1: How many sticks would you like to take? (1-3) "))
+        if player1_sticks:
+            sticks -= player1_sticks
+            print('{} sticks remaining'.format(sticks))
+        player2_sticks = int(input("Player 2: How many sticks would you like to take? (1-3) "))
+        if player2_sticks:
+            sticks -= player2_sticks
+            print('{} sticks remaining'.format(sticks))
+        if sticks <= 0:
+            x = input("Player {}, you lost. Enter yes to play again: ".format('x'))
+            if x == 'yes':
+                return main()
+            else:
+                exit()
+
+
+    # if sticks <= 0:
+    #     x = input('Game over.  Enter yes to play again: ')
+    #     if x == 'yes':
+    #         return main()
+    #     else:
+    #         exit()
+
+
+
+
+if __name__ == '__main__':
+    main()
